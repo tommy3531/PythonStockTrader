e6a0894a751a07e0607d366712517d9defa8a5d5
tommy3531 <thomas.marler@hotmail.com>
2018-09-14 20:37:06 -0500

    Added graph to represent moving avg and bar graph to represent volume

--- test.py
+++ test.py
@@ -7,12 +7,22 @@ import pandas as pd
 import pandas_datareader.data as web
 
 style.use('ggplot')
-state = dt.datetime(2000,1,1)
-end = dt.datetime(2016,12,31)
+state = dt.datetime(2000, 1, 1)
+end = dt.datetime(2016, 12, 31)
 
 df = web.DataReader('TSLA', 'yahoo', state, end)
-print(df.head(6))
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+print(df.head())
+# df.plot()
+# plt.show()
 
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+plt.show()
 
 # tsla = Stock('TSLA')
 # tsla.get_open()
@@ -20,4 +30,3 @@ print(df.head(6))
 
 # mydata = quandl.get("FRED/GDP")
 # print(mydata)
-
