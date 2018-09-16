84a9181777f919d5dc419e6c01d5786fba1874ff
tommy3531 <thomas.marler@hotmail.com>
2018-09-16 17:45:29 -0500

    Playing around with Alpha api and passing api keys as env

--- test.py
+++ test.py
@@ -5,24 +5,35 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+
 
 style.use('ggplot')
-state = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12, 31)
-
-df = web.DataReader('TSLA', 'yahoo', state, end)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+state = dt.datetime(2016, 1, 1)
+end = dt.datetime(2018, 12, 31)
+dotenv_path = join(dirname(__file__), 'apikey.env')
+load_dotenv(dotenv_path)
+alpha = os.getenv('ALPHAVANTAGE_API_KEY')
+# print(alpha)
+#
+df = web.DataReader('FB', 'av-daily', state, end, access_key=alpha)
 print(df.head())
-# df.plot()
+# df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+# print(df.head())
+# # df.plot()
+# # plt.show()
+#
+# ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+# ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+#
+# ax1.plot(df.index, df['Adj Close'])
+# ax1.plot(df.index, df['100ma'])
+# ax2.bar(df.index, df['Volume'])
 # plt.show()
-
-ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
-
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
-plt.show()
 
 # tsla = Stock('TSLA')
 # tsla.get_open()
